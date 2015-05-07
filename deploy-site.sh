4372860306262e124d2451ad3e6e24abc52bdcd7
Sune Simonsen <sune@we-knowhow.dk>
2015-05-07 13:39:58 +0200

    Added a deploy-site script

---
+++ deploy-site.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+CURRENT_BRANCH=`git symbolic-ref -q HEAD | sed -e 's|^refs/heads/||'`
+npm run generate-site
+git fetch origin gh-pages
+git checkout -B gh-pages origin/gh-pages
+rm `git ls-files | grep -v '^\.gitignore$'`
+if [ ! -f ".gitignore" ]; then
+    echo "node_modules" > .gitignore
+fi
+cp -r site-build/* .
+if [ "`git status --porcelain`" != "" ]; then \
+	(git add -A . && \
+	git commit -m "Updated site" && \
+	git push origin +gh-pages:gh-pages)
+fi
+git checkout $CURRENT_BRANCH
