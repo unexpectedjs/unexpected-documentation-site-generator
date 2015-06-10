746222bd78cb5f54e9fadd524701285d0be6a0ca
Sune Simonsen <sune@we-knowhow.dk>
2015-06-10 15:50:53 +0200

    make it possible to specify the target branch to deploy to

--- deploy-site.sh
+++ deploy-site.sh
@@ -4,16 +4,18 @@ then
     echo "Working tree is dirty, please commit or stash your changes, then try again"
     exit 1
 fi
+
+TARGET_BRANCH=${1:-gh-pages}
 CURRENT_BRANCH=`git symbolic-ref -q HEAD | sed -e 's|^refs/heads/||'`
 npm run generate-site
-git fetch origin gh-pages
-git checkout -B gh-pages origin/gh-pages
+git fetch origin $TARGET_BRANCH
+git checkout -B $TARGET_BRANCH origin/$TARGET_BRANCH
 rm `git ls-files | grep -v '^\.gitignore$'`
 printf "node_modules\nsite-build\n" > .gitignore
 cp -r site-build/* .
 if [ "`git status --porcelain`" != "" ]; then \
 	(git add -A . && \
 	git commit -m "Updated site" && \
-	git push origin +gh-pages:gh-pages)
+	git push origin +$TARGET_BRANCH:$TARGET_BRANCH)
 fi
 git checkout $CURRENT_BRANCH
