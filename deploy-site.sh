2bc81ab587f3116a098680443838f4829bacf1f8
Andreas Lind <andreas@one.com>
2015-07-29 02:15:29 +0200

    deploy-site.sh: Create gh-pages as an orphan branch if it doesn't exist.

--- deploy-site.sh
+++ deploy-site.sh
@@ -9,7 +9,12 @@ TARGET_BRANCH=${1:-gh-pages}
 CURRENT_BRANCH=`git symbolic-ref -q HEAD | sed -e 's|^refs/heads/||'`
 npm run generate-site
 git fetch origin $TARGET_BRANCH
-git checkout -B $TARGET_BRANCH origin/$TARGET_BRANCH
+if [ `git branch --list origin/$TARGET_BRANCH `]; then \
+    git checkout -B $TARGET_BRANCH origin/$TARGET_BRANCH
+else \
+    git checkout --orphan $TARGET_BRANCH && \
+    git rm -rf .
+fi
 rm `git ls-files | grep -v '^\.gitignore$'`
 printf "node_modules\nsite-build\n" > .gitignore
 cp -r site-build/* .
